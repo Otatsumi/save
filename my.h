82b8a15b81ac2d4d3363ad3d91e6cb413c199816
Otatsumi <jordan.Bauw@gmail.com>
2014-01-25 13:05:33 +0100

    Create my.h

---
+++ my.h
@@ -0,0 +1,11 @@
+#ifndef MY_H_
+# define MY_H_
+
+void    my_putchar(char c);
+void    my_putstr(char *str);
+int     my_strlen(char *str);
+void    my_put_nbr(int nb);
+void    my_swap(int *a, int *b);
+int     my_getnbr(char *str);
+
+#endif /* !MY_H_ */
